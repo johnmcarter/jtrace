593f065b8cb2560fe73131b93a178ddbdfa05c5f
johnmcarter <jmcarter211@gmail.com>
2024-07-01 19:17:30 -0400

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Author: John Carter
-# Created: 2021/12/15 18:47:43
-# Last modified: 2022/01/21 20:05:36
+# Created: 2021/12/15 
+# Last modified: 2024/07/01 
 # Install jtrace on a Linux machine
-
 
 RED=$'\e[1;31m'
 GRN=$'\e[1;32m'
 END=$'\e[0m'
 
 help() {
-   echo "[${RED}ERROR${END}] USAGE: $0 <qualified jtrace source code location>"
-   exit 1
+    echo "[${RED}ERROR${END}] USAGE: $0 <qualified jtrace source code location>"
+    exit 1
 }
 
 exit_process() {
@@ -23,31 +21,29 @@ exit_process() {
 trap exit_process INT TERM
 
 if (( EUID != 0 )); then
-   echo "[${RED}ERROR${END}] This script must be run as root" 
-   exit 1
+    echo "[${RED}ERROR${END}] This script must be run as root" 
+    exit 1
 fi
 
 if [[ $# -ne 1 ]]; then
     help
 fi
 
-echo "[${GRN}INFO${END}] Entering directory '$1'"
-cd $1
+source_dir="$1"
+
+echo "[${GRN}INFO${END}] Entering directory '$source_dir'"
+cd "$source_dir" || { echo "[${RED}ERROR${END}] Failed to enter directory '$source_dir'"; exit 1; }
 
 echo "[${GRN}INFO${END}] Compiling jtrace executable"
-make
+make || { echo "[${RED}ERROR${END}] Compilation failed"; exit 1; }
 
 echo "[${GRN}INFO${END}] Installing jtrace as an executable script in /usr/bin"
 cat > /usr/bin/jtrace <<EOF
 #!/bin/bash
 
-${1}/jtrace \$1
+"${source_dir}/jtrace" "\$@"
 EOF
 
-chmod +x /usr/bin/jtrace
+chmod +x /usr/bin/jtrace || { echo "[${RED}ERROR${END}] Failed to make /usr/bin/jtrace executable"; exit 1; }
 
-if [[ $? -ne 0 ]]; then
-    echo "[${RED}SUCCESS${END}] jtrace installation failed"
-else 
-    echo "[${GRN}SUCCESS${END}] jtrace installation successful!"
-fi
+echo "[${GRN}SUCCESS${END}] jtrace installation successful!"
